59e1d03b42cab98fab32384f736c1245021a51d0
B00156641 Neimanas Sarmukas <b00156641@mytudublin.ie>
2024-11-12 15:48:39 +0000

    part3

--- team.php
+++ team.php
@@ -1,48 +1,39 @@
 <?php
 
-class Team {
-
+class Team
+{
     private $teamName;
     private $totalPoints = 0;
     private $totalGoals = 0;
     private $totalGames = 0;
 
- 
-    public function __construct($teamName) {
+    public function __construct($teamName)
+    {
         $this->teamName = $teamName;
+        $this->totalPoints = 0;
+        $this->totalGames = 0;
+        $this->totalGoals = 0;
     }
 
-
-    public function finalScore($homeScore, $opposingScore) {
+    public function finalScore($homeScore, $opposingScore)
+    {
         $this->totalGames += 1;
-
         $this->totalGoals += $homeScore;
 
-        if ($homeScore > $opposingScore) {
+        if ($homeScore > $opposingScore)
+        {
             $this->totalPoints += 3;
-        } elseif ($homeScore == $opposingScore) {
+        }
+        elseif ($homeScore == $opposingScore)
+        {
             $this->totalPoints += 1;
         }
     }
 
-    public function getTeamName() {
-        return $this->teamName;
-    }
-
-    public function getTotalPoints() {
-        return $this->totalPoints;
-    }
-
-    public function getTotalGoals() {
-        return $this->totalGoals;
-    }
-
-    public function getTotalGames() {
-        return $this->totalGames;
-    }
-
-    public function getGoalAverage() {
-        if ($this->totalGames === 0) {
+    public function getGoalAverage()
+    {
+        if ($this->totalGames === 0)
+        {
             return 0;
         }
         return $this->totalGoals / $this->totalGames;
--- testPart2.php
+++ testPart2.php
@@ -12,10 +12,6 @@ $team->finalScore(3, 3);
 $team->finalScore(1, 1); 
 
 // Display the results
-echo "Team Name: " . $team->getTeamName() . PHP_EOL;
-echo "Total Points: " . $team->getTotalPoints() . PHP_EOL;
-echo "Total Goals: " . $team->getTotalGoals() . PHP_EOL;
-echo "Total Games: " . $team->getTotalGames() . PHP_EOL;
-echo "Goal Average: " . $team->getGoalAverage() . PHP_EOL;
+echo "Goal Average: " . $team->getGoalAverage()
 
 ?>
